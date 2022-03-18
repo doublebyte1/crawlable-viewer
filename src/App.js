35e5d01e45c47c6d61deee32779ee10087874694
doublebyte1 <info@doublebyte.net>
2022-03-18 19:24:57 +0100

    - Added initial page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const url="http://localhost:9000/crawlable_catalogue/";
+
+
 
 function App() {
+  const [data, setData] = useState({ links: [] });
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function fetchData() {
+      const result = await axios( url + "collections.json");
+      if (!ignore) setData(result.data);
+    }
+
+    fetchData();
+    return () => { ignore = true; }
+  });
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+      <div className="container">
+        <div className="jumbotron bg-dark text-white">
+          <h1>{data.title}</h1>      
+          <p>{data.description}.</p>
+        </div>
+ 
+
+        <div className="row">
+
+        {data.links.map((item,index) => (
+
+        <div className="col-sm-6" key={index}>
+          <div className="card">
+            <div className="card-body">
+
+              <h5 className="card-title">{item.href.substring(0, item.href.lastIndexOf('/'))}</h5>
+              <p className="card-text">Click bellow to explore this record collection.</p>
+              <a href={url + item.href}  className="btn btn-primary">Open collection</a>
+            </div>
+          </div>
+        </div>
+
+      ))
+      }
+
+        </div>
+
+      <br></br>
+        <footer className="mt-auto">
+          <a href="https://byteroad.net">&copy; ByteRoad 2022</a>
+        </footer>
+
+      </div> 
+
+
+
+
+      
   );
 }
 
