63b51c69b32d2d4b948a5355138b8089591e5cf6
doublebyte1 <info@doublebyte.net>
2022-08-03 12:49:20 +0200

    - updates

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ const url="http://localhost:9000/crawlable_catalogue/";
 function App() {
   const [data, setData] = useState({ links: [] });
 
+   console.log(window.location.href);
+
   useEffect(() => {
     let ignore = false;
 
     async function fetchData() {
       const result = await axios( url + "collections.json");
-      if (!ignore) setData(result.data);
+      if (!ignore) {
+        setData(result.data);
+      }
     }
 
     fetchData();
@@ -22,12 +26,7 @@ function App() {
   });
 
 
-
-
-
   return (
-
-
       <div className="container">
         <div className="jumbotron bg-dark text-white">
           <h1>{data.title}</h1>      
@@ -39,16 +38,18 @@ function App() {
 
         {data.links.map((item,index) => (
 
+        item.type === 'text/html' && item.rel ==='item' ?          
         <div className="col-sm-6" key={index}>
           <div className="card">
             <div className="card-body">
 
-              <h5 className="card-title">{item.href.substring(0, item.href.lastIndexOf('/'))}</h5>
+              <h5 className="card-title">{item.title}</h5>
               <p className="card-text">Click bellow to explore this record collection.</p>
               <a href={url + item.href}  className="btn btn-primary">Open collection</a>
             </div>
           </div>
         </div>
+        : <div key={index}> </div>
 
       ))
       }
@@ -68,5 +69,6 @@ function App() {
       
   );
 }
+
 
 export default App;
